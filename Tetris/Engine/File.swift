1edefa6516b28e5f59ef6fe6912b0638799c8b22
Vladislav Morozov <moroffv@gmail.com>
2023-07-16 16:27:31 +0300

    Put entities to separate files

--- Tetris/Engine/File.swift
+++ Tetris/Engine/File.swift
@@ -1,77 +1,17 @@
 import Foundation
 import UIKit
 
-protocol GameLauncherDelegate: AnyObject {
-    func didUpdate(string: String)
-}
 
-final class GameLauncher {
-
-    private let loopTimer: LoopTimer
-    private let playfield: Playfield
-    private let shapeFactory: ShapeFactory
+enum ShapeType {
     
-    weak var delegate: GameLauncherDelegate?
-        
-    init() {
-        self.loopTimer = LoopTimer()
-        self.shapeFactory = ShapeFactory()
-        self.playfield = Playfield(
-            config: PlayfieldConfiguration.default,
-            shapeFactory: shapeFactory
-        )
-        
-        loopTimer.delegate = playfield
-        playfield.delegate = self
-    }
-    
-    func start() {
-        loopTimer.startTimer()
-    }
-
-    func end() {
-        loopTimer.stopTimer()
-    }
-    
-    func clean() {
-        loopTimer.stopTimer()
-        playfield.setupField()
-        loopTimer.startTimer()
-    }
-    
-    //TODO: try to find better relationships
-    func leftDidTapped() {
-        playfield.move(x: -1)
-    }
-    
-    func rightDidTapped() {
-        playfield.move(x: 1)
-    }
-    
-    func rotateDidTapped() {
-        playfield.rotate()
-    }
-}
-
-extension GameLauncher: PlayfieldDelegate {
-    func playfieldGameEnded() {
-        loopTimer.stopTimer()
-    }
-    
-    //TODO: do better
-    func didRedraw(string: String) {
-        delegate?.didUpdate(string: string)
-    }
 }
 
 struct Shape {
     let points: [CGPoint]
     var offset: CGPoint = CGPoint(x: 5, y: 0)
     
+    //TODO: refactor
     func rotated() -> Shape {
-//        let centerX = Int(round(points.reduce(0) { $0 + $1.x } / CGFloat(points.count)))
-//        let centerY = Int(round(points.reduce(0) { $0 + $1.y } / CGFloat(points.count)))
-        
         let centerX: CGFloat = points[1].x
         let centerY: CGFloat = points[1].y
         
@@ -104,20 +44,20 @@ final class ShapeFactory {
     
     // TODO: make enum?
     func generate() -> Shape {
-        switch Int.random(in: 0..<5) {
-        case 0:
+//        switch Int.random(in: 0..<5) {
+//        case 0:
             return I()
-        case 1:
-            return L()
-        case 2:
-            return T()
-        case 3:
-            return S()
-        case 4:
-            return square()
-        default:
-            fatalError()
-        }
+//        case 1:
+//            return L()
+//        case 2:
+//            return T()
+//        case 3:
+//            return S()
+//        case 4:
+//            return square()
+//        default:
+//            fatalError()
+//        }
     }
 
     func I() -> Shape {
@@ -166,213 +106,6 @@ final class ShapeFactory {
     }
 }
 
-protocol PlayfieldDelegate: AnyObject {
-    func playfieldGameEnded()
-    func didRedraw(string: String)
-}
-
-final class Playfield {
-    private var field: Array<Array<Bool>> = []
-    private var presentationField: Array<Array<Bool>> = []
-
-    private var currentShape: Shape = Shape(points: [])
-    
-    private let config: PlayfieldConfiguration
-    private let shapeFactory: ShapeFactory
-    
-    weak var delegate: PlayfieldDelegate?
-        
-    init(config: PlayfieldConfiguration, shapeFactory: ShapeFactory) {
-        self.config = config
-        self.shapeFactory = shapeFactory
-        
-        setupField()
-    }
-
-    func update(with countdown: Int) {
-        moveDown()
-    }
-    
-    func moveDown() {
-        if !doesCollide(shape: currentShape, xOffset: currentShape.offset.x, yOffset: currentShape.offset.y + 1) {
-            currentShape.offset.y += 1;
-        } else {
-            placeShapeOnField()
-        }
-        redraw()
-    }
-    
-    func move(x: CGFloat) {
-        if !doesCollide(shape: currentShape, xOffset: currentShape.offset.x + x, yOffset: currentShape.offset.y) {
-            currentShape.offset.x += x;
-            redraw()
-        }
-    }
-    
-    func rotate() {
-        let rotatedShape = currentShape.rotated()
-        if !doesCollide(shape: rotatedShape, xOffset: rotatedShape.offset.x, yOffset: rotatedShape.offset.y) {
-            currentShape = rotatedShape
-            redraw()
-        }
-    }
-        
-    func doesCollide(shape: Shape, xOffset: CGFloat, yOffset: CGFloat) -> Bool {
-        for point in shape.points {
-            let x = Int(point.x + xOffset)
-            let y = Int(point.y + yOffset)
-            
-            if y >= config.height {
-                return true
-            }
-            
-            if !(0..<config.width).contains(x) {
-                return true
-            }
-                        
-            if field[x][y] {
-                return true
-            }
-        }
-        return false
-    }
-    
-    //TODO: refactor
-    func redraw() {
-        presentationField = field
-        
-        for point in currentShape.points {
-            let x = Int(point.x + currentShape.offset.x)
-            let y = Int(point.y + currentShape.offset.y)
-            
-            presentationField[x][y] = true
-        }
-        var transpondedPresentationField: Array<Array<Bool>> = []
-        
-        for _ in 0..<config.height {
-            transpondedPresentationField.append(Array(repeating: false, count: config.width))
-        }
-        
-        for i in 0..<presentationField.count {
-            for j in 0..<presentationField[i].count {
-                transpondedPresentationField[j][i] = presentationField[i][j]
-            }
-        }
-
-        var result: String = ""
-        for i in 0..<transpondedPresentationField.count {
-            var row = ""
-            for j in 0..<transpondedPresentationField[i].count {
-                let element = transpondedPresentationField[i][j]
-                if element {
-                    row.append("■")
-                } else {
-                    row.append("□")
-                }
-            }
-            print(row)
-            result.append("\(row)\n")
-            row = ""
-        }
-//        print("\n")
-        
-        delegate?.didRedraw(string: result)
-    }
-    
-    func placeShapeOnField() {
-        for point in currentShape.points {
-            let x = Int(point.x + currentShape.offset.x)
-            let y = Int(point.y + currentShape.offset.y)
-            
-            field[x][y] = true
-        }
-        
-        removeRowsIfNeeded()
-        
-        currentShape = shapeFactory.generate()
-        
-        //TODO: adapt to config
-        if doesCollide(shape: currentShape, xOffset: 5, yOffset: 0) {
-            delegate?.playfieldGameEnded()
-        }
-    }
-    
-//    func removeRowsIfNeeded() {
-//        var spaceBetweenColumns = false
-//        for j in (0..<config.height).reversed() {
-//            for i in 0..<config.width {
-//                spaceBetweenColumns = !field[i][j]
-//                if spaceBetweenColumns {
-//                    break
-//                }
-//            }
-//            if !spaceBetweenColumns {
-//                removeRow(row: j)
-//            }
-//        }
-//    }
-    
-    
-    func removeRowsIfNeeded() {
-        var spaceBetweenColumns = false
-        var j = 20
-        while j >= 1 {
-            j -= 1
-            spaceBetweenColumns = false
-            for i in 0...9 {
-                if field[i][j] == false {
-                    spaceBetweenColumns = true
-                    break
-                }
-            }
-            if !spaceBetweenColumns {
-                removeRow(row: j)
-                j += 1
-            }
-        }
-    }
-
-    
-    private func removeRow(row: Int) {
-        for j in (0..<row).reversed() {
-            for i in 0...9 {
-                field[i][j + 1] = field[i][j]
-            }
-        }
-    }
-    
-//    private func removeRow(row: Int) {
-//        for j in 0...row {
-//            for i in 0..<config.width {
-//                field[i][j] = field[i][j - 1]
-//            }
-//        }
-//    }
-    
-    func setupField() {
-        field = []
-        for _ in 0..<config.width {
-            field.append(Array(repeating: false, count: config.height))
-        }
-        presentationField = field
-        currentShape = shapeFactory.generate()
-    }
-}
-
-extension Playfield: LoopTimerDelegate {
-    func loopTimerDidFire(_ loopTimer: LoopTimer, with countdown: Int) {
-        update(with: countdown)
-    }
-}
-
-struct PlayfieldConfiguration {
-    let width: Int
-    let height: Int
-}
-
-extension PlayfieldConfiguration {
-    static let `default` = PlayfieldConfiguration(width: 10, height: 20)
-}
 
 // What to do on left/right/rotate
 protocol InputHandler: AnyObject {
--- Tetris/Engine/LoopTimer.swift
+++ Tetris/Engine/LoopTimer.swift
@@ -32,43 +32,8 @@ final class LoopTimer {
     }
     
     @objc private func timerFired() {
+        //TODO :do i need countdown?
         countdown += 1
         delegate?.loopTimerDidFire(self, with: countdown)
     }
 }
-
-
-////TODO: is there a way to not rely on CADisplayLink?
-//final class LoopTimer {
-//
-//    private var countdown: Int = 0
-//    private var displayLink: CADisplayLink?
-//    //TODO: why i use display link?
-//
-//    weak var delegate: LoopTimerDelegate?
-//
-//    func initTimer() {
-//        let displayLink = CADisplayLink(target: self, selector: #selector(timerFired))
-//        self.displayLink = displayLink
-//        displayLink.preferredFramesPerSecond = 30
-//        displayLink.isPaused = true
-//        displayLink.add(to: RunLoop.current, forMode: RunLoop.Mode.default)
-//    }
-//
-//    func startTimer() {
-//        displayLink?.isPaused = false
-//    }
-//
-//    func stopTimer() {
-//        displayLink?.isPaused = true
-//    }
-//
-//    deinit {
-//        displayLink?.remove(from: RunLoop.current, forMode: RunLoop.Mode.default)
-//    }
-//
-//    @objc private func timerFired() {
-//        countdown += 1
-//        delegate?.loopTimerDidFire(self, with: countdown)
-//    }
-//}
